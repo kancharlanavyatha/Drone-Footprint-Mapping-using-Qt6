8c269db14b18f889fc20035c89c36b92062a2e69
Navyatha <lakshminavyatha14@gmail.com>
2026-09-09 11:51:30 +0530

    Add open_in_qgis.bat launcher to pre-load footprints and DEM in QGIS automatically

---
+++ open_in_qgis.bat
@@ -0,0 +1,5 @@
+@echo off
+echo =======================================================
+echo Opening Brighton Beach Footprints and 3D DEM in QGIS...
+echo =======================================================
+start "" "C:\Program Files\QGIS 3.44.12\bin\qgis-ltr.bat" "d:\DRDO\drone points\dsm.tif" "d:\DRDO\drone points\footprints_pipeline.geojson"
